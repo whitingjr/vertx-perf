82752b890786cc3e2f6bf3f7601318aed934778f
Jeremy Whiting <jwhiting@redhat.com>
2017-03-24 12:32:12 +0000

    Added body handler

--- src/main/java/io/vertx/perf/web/RouterHttpVariableBodySize.java
+++ src/main/java/io/vertx/perf/web/RouterHttpVariableBodySize.java
@@ -2,10 +2,11 @@ package io.vertx.perf.web;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.BodyHandler;
 
 public class RouterHttpVariableBodySize extends AbstractVerticle {
 
-   
+
    @Override
    public void start() throws Exception {
       String host = System.getProperty("vertx.host", "f24lite");
@@ -14,9 +15,12 @@ public class RouterHttpVariableBodySize extends AbstractVerticle {
       System.out.println("Port: " + port);
 
       Router router = Router.router(vertx);
-      
+      router.route().handler(BodyHandler.create());
+      router.route().handler(routingContext -> {
+         routingContext.response().putHeader("content-type", "text/html");
+      });
       router.post("/").handler(req -> {
-         req.response().putHeader("content-type", "text/html").setStatusCode(200).end("you used the wrong path, try /form instead");
+         req.response().setStatusCode(200).end("you used the wrong path, try /form instead");
       });
       router.post("/form").blockingHandler(routingContext -> {
          int size = routingContext.getBody().length();
@@ -25,9 +29,6 @@ public class RouterHttpVariableBodySize extends AbstractVerticle {
          routingContext.response().setStatusCode(200).end("<html><body><h1>Thank you for the message!</h1></body></html>");
          routingContext.next();
       }, false);
-      router.route().handler(routingContext -> {
-          routingContext.response().putHeader("content-type", "text/html").end("<html><head></head><body>This catch all handler is an indication you used the wrong method. Try POST with the /form URI.</body></html>");;
-       });
       vertx.createHttpServer().requestHandler(router::accept).listen(port, host);
    }
 }
