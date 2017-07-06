7eeace3396d8b5816e19a162037e000c0fdc59bb
Jeremy Whiting <jwhiting@redhat.com>
2017-07-06 14:24:13 +0100

    Modified to avoid getting the body as a string. Used more efficient headers.

--- src/main/java/io/vertx/perf/web/RouterHttpVariableBodySize.java
+++ src/main/java/io/vertx/perf/web/RouterHttpVariableBodySize.java
@@ -14,9 +14,9 @@ import io.vertx.ext.web.handler.BodyHandler;
 public class RouterHttpVariableBodySize extends AbstractVerticle {
 
    private static final Logger logger = Logger.getLogger(RouterHttpVariableBodySize.class.getName());
-   private static final String message = "<html><body><h1>Thank you for the message!</h1></body></html>";
-   private static final String TEXT_HTML = "text/html";
-   private static final String UTF_8 = "UTF-8";
+   private static final CharSequence UTF_8 = HttpHeaders.createOptimized("UTF-8");
+//   private static final CharSequence message = HttpHeaders.createOptimized("<html><body><h1>Thank you for the message!</h1></body></html>");
+   private static final Buffer message = 
 
    @Override
    public void start() throws Exception {
@@ -33,21 +33,20 @@ public class RouterHttpVariableBodySize extends AbstractVerticle {
       router.route().handler(BodyHandler.create());
       router.route().failureHandler(fh -> {
           logger.severe("Failure:" + fh.failure().getMessage());
-          fh.response().putHeader("Content-Type", "text/html");
+          fh.response().putHeader(HttpHeaders.CONTENT_TYPE, HttpHeaders.TEXT_HTML);
           fh.response().setStatusCode(500).end(String.format("barf [%1$s]",fh.failure().getMessage()));
        });
       router.post("/nonblockingform").blockingHandler(routingContext -> {
          setExceptionHandlers(routingContext);
          Buffer b = routingContext.getBody();
-         String body = routingContext.getBodyAsString(UTF_8);
-         int size = body.length();
-         int bs = Integer.parseInt(routingContext.request().getHeader("Content-Length"));
+         int size = b.length();
+         int bs = Integer.parseInt(routingContext.request().getHeader(HttpHeaders.CONTENT_LENGTH));
          boolean equal = size == bs;
-         assert size == bs: String.format("Comparing the size of the body string size [%1$d] and content length [%2$d] showed a mismatch. They should be identical.",  size, body.length());
+         assert equal: String.format("Comparing the size of the body string size [%1$d] and content length [%2$d] showed a mismatch. They should be identical.",  size, bs);
          if (equal){
-            routingContext.response().putHeader("Content-Type", TEXT_HTML);
+            routingContext.response().putHeader(HttpHeaders.CONTENT_TYPE, HttpHeaders.TEXT_HTML);
             routingContext.response().setStatusCode(200);
-            routingContext.response().end(message);
+            routingContext.response().end();
          } else {
             routingContext.fail(new Exception("Server detected the request body does not match the expected length as content-length indicates."));
             routingContext.next();
